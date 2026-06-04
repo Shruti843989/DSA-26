bdc42f41113b841f1917d50db64851e64b08d035
Shruti Aggarwal <aggarwalshruti423@gmail.com>
2026-06-04 23:46:22 +0530

    Add files via upload

--- PATTERNS/pattern10.java
+++ PATTERNS/pattern10.java
@@ -7,7 +7,9 @@ public class pattern10 {
      Scanner sct = new Scanner(System.in);
      int rows = sct.nextInt();
       for(int i =1;i<=rows;i++){
-        for(int j =i;j<=rows;j++){
+        // for(int j =i;j<=rows;j++){
+        // for(int j=rows;j>=i;j--){ NOT STANDARD TEMPLATE
+        for(int j=1;j<=rows+1-i;j++){
             System.out.print("*"+" ");
         }
         System.out.println();
---
+++ PATTERNS/pattern15.java
@@ -0,0 +1,8 @@
+package PATTERNS;
+
+public class pattern15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+    }
+}
